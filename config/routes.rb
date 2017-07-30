8be538f4f8ae0d963637c0bcb9ea3954996022b3
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-07-30 19:28:58 +0800

    Add Category resource for student

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :flash_cards, only: [:show]
+  resources :categories, only: [:show, :index]
 
   namespace :teacher do
     resources :flash_cards
     resources :categories, only: [:show, :create, :index]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
