02c75547a4cab662859b6fe70c4e8df45e5ccab0
Myk <mykola.kyryk@gmail.com>
2017-08-06 13:05:12 +0800

    Enable CORS.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :categories, only: [:show, :index]
   resources :learning_sessions, only: [:create]
 
+  get '/status.json', to: 'status#show'
+
   namespace :teacher do
     resources :flash_cards
     resources :categories, only: [:show, :create, :index]
