2d2d6559e5380a74487e774d982c92ef7508a3f6
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-08-08 17:35:47 +0800

    Move learning session test from request to acceptance tests.

---
+++ spec/acceptance/learning_sessions_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "LearningSessions" do
+  post '/learning_sessions' do
+    parameter :category_id, "Category ID"
+
+    let!(:flash_cards) { create :flash_card, category: category }
+    let(:category) { create :category }
+    let(:response_json) do
+      {flash_card_ids: [flash_cards.id]}.to_json
+    end
+    let(:params) { {category_id: category.id} }
+
+    example 'creates new learning session and returns flash_card_ids' do
+      do_request(params)
+      expect(status).to eq 200
+      expect(response_body).to eq response_json
+    end
+  end
+end
