3495ceaf58d598cc63405084a9bd980afa0dbd52
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-12-05 15:41:12 +0800

    Scope categories and flash cards by current user.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,18 @@
 class CategoriesController < ApplicationController
   def index
-    categories = Category.all
+    categories = current_user.categories
 
     render json: CategoriesSerializer.new(categories).to_json, status: :ok
   end
 
   def show
-    category = Category.find(params[:id])
+    category = current_user.categories.find(params[:id])
 
     render json: CategoryCommonSerializer.new(category, category.flash_cards).to_json, status: :ok
   end
 
   def create
-    category = Category.new(category_params).tap { |cat| cat.user_id = current_user.id }
+    category = current_user.categories.new(category_params)
 
     if category.save
       render json: CategorySerializer.new(category).to_json, status: :created
--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -1,18 +1,18 @@
 class FlashCardsController < ApplicationController
   def index
-    flash_cards = FlashCard.all
+    flash_cards = current_user.flash_cards
 
     render json: FlashCardsSerializer.new(flash_cards).to_json
   end
 
   def show
-    flash_card = FlashCard.find(params[:id])
+    flash_card = current_user.flash_cards.find(params[:id])
 
     render json: FlashCardSerializer.new(flash_card).to_json
   end
 
   def create
-    flash_card = FlashCard.new(flash_card_params).tap { |flash_crd| flash_crd.user_id = current_user.id }
+    flash_card = current_user.flash_cards.new(flash_card_params).tap { |flash_crd| flash_crd.user_id = current_user.id }
 
     if flash_card_params[:category_id].nil?
       flash_card.category = current_user.categories.find_by_is_default(true) || current_user.categories.create(name: "General", is_default: true)
@@ -26,7 +26,7 @@ class FlashCardsController < ApplicationController
   end
 
   def destroy
-    flash_card = FlashCard.find(params[:id])
+    flash_card = current_user.flash_cards.find(params[:id])
     flash_card.destroy
 
     render :ok
