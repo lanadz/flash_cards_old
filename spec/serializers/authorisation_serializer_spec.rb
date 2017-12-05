b0683d9b5f909db9380bb1e2b56900a9814e48d9
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-12-05 16:03:39 +0800

    Fix rest cards

--- spec/serializers/authorisation_serializer_spec.rb
+++ spec/serializers/authorisation_serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AuthorisationSerializer do
-  let(:user) { create :student }
+  let(:user) { create :user }
 
   describe '#to_json' do
     subject { described_class.new(user, 'token') }
--- spec/services/create_token_spec.rb
+++ spec/services/create_token_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CreateToken do
-  let(:student) { create :student, auth_token: nil, auth_token_expired_at: nil }
+  let(:user) { create :user, auth_token: nil, auth_token_expired_at: nil }
 
-  subject { described_class.new(student) }
+  subject { described_class.new(user) }
 
   it "generates token and expiry_date and updates user's fields" do
     subject.execute
-    student.reload
-    expect(student.auth_token).to be_present
-    expect(student.auth_token_expired_at).to be_present
+    user.reload
+    expect(user.auth_token).to be_present
+    expect(user.auth_token_expired_at).to be_present
   end
 end
