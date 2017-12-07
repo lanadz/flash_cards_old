8e2bcfa029cd354a113e0d509e3a489a428bb374
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-12-07 09:40:00 +0800

    Hide registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :flash_cards, only: [:show, :index, :create, :destroy]
   resources :categories, only: [:show, :index, :create]
   resources :learning_sessions, only: [:create]
-  resources :registrations, only: [:create]
+  # resources :registrations, only: [:create]
   resources :sessions, only: [:create] do
     collection do
       delete :signout
