c73b9d23ba50ee213fb841b1ff881ac5f91273ad
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-02-25 15:09:26 +0200

    Update indexes

--- db/migrate/20180116030023_create_flash_card_shows.rb
+++ db/migrate/20180116030023_create_flash_card_shows.rb
@@ -1,12 +1,10 @@
 class CreateFlashCardShows < ActiveRecord::Migration[5.1]
   def change
     create_table :flash_card_shows do |t|
-      t.bigint :user_id
-      t.bigint :flash_card_id
+      t.bigint :user_id, index: true
+      t.bigint :flash_card_id, index: true
       t.integer :show_times
       t.integer :correct_times, index: true
     end
-
-    add_index :flash_card_shows, [:user_id, :flash_card_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,8 @@ ActiveRecord::Schema.define(version: 20180116030023) do
     t.integer "show_times"
     t.integer "correct_times"
     t.index ["correct_times"], name: "index_flash_card_shows_on_correct_times"
-    t.index ["user_id", "flash_card_id"], name: "index_flash_card_shows_on_user_id_and_flash_card_id"
+    t.index ["flash_card_id"], name: "index_flash_card_shows_on_flash_card_id"
+    t.index ["user_id"], name: "index_flash_card_shows_on_user_id"
   end
 
   create_table "flash_cards", force: :cascade do |t|
