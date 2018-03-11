e9b896e35ef078bc684af1795ae36d13bd770aae
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-03-11 19:42:54 +0200

    Place cards to boxes

--- app/services/default_strategy.rb
+++ app/services/default_strategy.rb
@@ -2,6 +2,7 @@ class DefaultStrategy
   def initialize(flash_cards, number_of_cards = 10)
     @flash_cards = flash_cards
     @number_of_cards = number_of_cards
+    move_cards_to_boxes
   end
 
   def cards
@@ -10,7 +11,13 @@ class DefaultStrategy
 
   private
 
-  attr_reader :flash_cards, :number_of_cards
+  attr_reader :flash_cards,
+              :number_of_cards,
+              :cards_box_0,
+              :cards_box_1,
+              :cards_box_2,
+              :cards_box_3,
+              :cards_box_4
 
   def shuffle(length)
     ENV.fetch('BOX1').to_f
@@ -21,6 +28,21 @@ class DefaultStrategy
         "show_times" => card.flash_card_show&.show_times || 0,
         "box" => card.flash_card_show&.box || 0
       )
+    end
+  end
+
+  def move_cards_to_boxes
+    @cards_box_0 = Array.new
+    @cards_box_1 = Array.new
+    @cards_box_2 = Array.new
+    @cards_box_3 = Array.new
+    @cards_box_4 = Array.new
+    flash_cards.each do |flash_card|
+      @cards_box_0 << flash_card if flash_card.flash_card_show&.box.nil?
+      @cards_box_1 << flash_card if flash_card.flash_card_show&.box == 1
+      @cards_box_2 << flash_card if flash_card.flash_card_show&.box == 2
+      @cards_box_3 << flash_card if flash_card.flash_card_show&.box == 3
+      @cards_box_4 << flash_card if flash_card.flash_card_show&.box == 4
     end
   end
 
--- spec/services/default_strategy_spec.rb
+++ spec/services/default_strategy_spec.rb
@@ -33,6 +33,51 @@ RSpec.describe DefaultStrategy do
         expect(subject.cards.length).to eq 2
       end
     end
+
+    context 'move cards to boxes correctly' do
+      let(:card_box_0) do
+        flash_card = build :flash_card, id: 1, user: user, category: category
+        flash_card.stub_chain(:flash_card_show, :box).and_return(nil)
+        flash_card
+      end
+
+      let(:card_box_1) do
+        flash_card = build :flash_card, id: 2, user: user, category: category
+        flash_card.stub_chain(:flash_card_show, :box).and_return(1)
+        flash_card
+      end
+
+      let(:card_box_2) do
+        flash_card = build :flash_card, id: 3, user: user, category: category
+        flash_card.stub_chain(:flash_card_show, :box).and_return(2)
+        flash_card
+      end
+
+      let(:card_box_3) do
+        flash_card = build :flash_card, id: 4, user: user, category: category
+        flash_card.stub_chain(:flash_card_show, :box).and_return(3)
+        flash_card
+      end
+
+      let(:card_box_4) do
+        flash_card = build :flash_card, id: 5, user: user, category: category
+        flash_card.stub_chain(:flash_card_show, :box).and_return(4)
+        flash_card
+      end
+
+      let(:boxed_flash_cards) {[card_box_0,card_box_1,card_box_2,card_box_3,card_box_4]}
+
+      describe '#new' do
+        subject { described_class.new(boxed_flash_cards) }
+        it '#move_cards_to_boxes' do
+          expect(subject.send(:cards_box_0)).to eq [card_box_0]
+          expect(subject.send(:cards_box_1)).to eq [card_box_1]
+          expect(subject.send(:cards_box_2)).to eq [card_box_2]
+          expect(subject.send(:cards_box_3)).to eq [card_box_3]
+          expect(subject.send(:cards_box_4)).to eq [card_box_4]
+        end
+      end
+    end
   end
 
 end
