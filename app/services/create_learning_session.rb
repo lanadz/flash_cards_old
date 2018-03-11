a38e46d5bd4a515ee5e0d180014c03015052d274
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-03-11 13:58:35 +0200

    I need cards with flash_cards_show info included for DefaultStrategy. I don’t want to have DB dependancy in the strategy class.

--- app/services/create_learning_session.rb
+++ app/services/create_learning_session.rb
@@ -5,9 +5,8 @@ class CreateLearningSession
   end
 
   def execute
-    cards = DefaultStrategy.new(flash_cards).cards
-    flash_card_shows = FlashCardShow.where(user: user).where(flash_card: cards)
-    combined_cards = cards.map do |card|
+    flash_card_shows = FlashCardShow.where(user: user).where(flash_card: flash_cards)
+    cards = flash_cards.map do |card|
       result = flash_card_shows.find { |flash_card_show| card.id == flash_card_show.flash_card_id }
       card.attributes.merge(
         "correct_times" => result&.correct_times || 0,
@@ -15,6 +14,8 @@ class CreateLearningSession
         "box" => result&.box || 0
       )
     end
+
+    combined_cards = DefaultStrategy.new(cards).cards
     {
       learning_session_details: LearningSessionDetail.create!(
         user: user,
