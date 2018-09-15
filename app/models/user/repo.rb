f73657a21039a4c3d56e3e0ce46a1a1d448737ea
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-09-15 15:58:15 +0800

    Refactor User saving - Fill errors array

--- app/models/user/repo.rb
+++ app/models/user/repo.rb
@@ -10,20 +10,15 @@ class User
     end
 
     def execute
-      begin
-        User.transaction do
-          @user = User.new(params)
+      User.transaction do
+        @user = User.new(params)
+        @category = @user.categories.new(name: "General", is_default: true)
 
-          if user.save
-            @category = user.categories.new(name: "General", is_default: true)
-            raise ActiveRecord::Rollback unless @category.save
-          else
-            raise ActiveRecord::Rollback
-          end
+        if !user.save || !category.save
+          @errors << user.errors if user.errors.present?
+          @errors << category.errors if category.errors.present?
+          raise ActiveRecord::Rollback
         end
-      rescue ActiveRecord::Rollback
-        @errors << user.errors if user.errors.present?
-        @errors << category.errors if category.errors.present?
       end
 
       self
